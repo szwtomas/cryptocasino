f6386f696a57e14eadcc2a8d129c0909d80ae0e6
merivazquezn <mvazquezn@fi.uba.ar>
2021-11-28 13:25:42 -0300

    CryptoRoulette

--- casino_app/contracts/CryptoRoulette.sol
+++ casino_app/contracts/CryptoRoulette.sol
@@ -1,41 +1,106 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
-
-import "./CryptoCraps.sol";
-import "./Randomizable.sol";
+import "./CryptoCasinoInterface.sol";
 import "./CryptoGame.sol";
 
-contract CryptoRoulette is CryptoGame, OnlyValidFunds {
+contract CryptoRoulette is CryptoGame {
     
-    //TODO: Ver si 256 no es mucho (si)
-    struct PlayerBets {
-        address playerAddress;
-        mapping(uint8 => uint256) numbers;
-        mapping(uint8 => uint256) colors; 
-        //mapping(uint8 => uint256) evens;
+    struct NumberBet {
+      uint8 number;
+      uint256 bet;
     }
 
+    enum BetColor {Red, Blue}
 
-    address private _casinoAddress;
-    PlayerBets[] private roulettePlayers;
+    struct ColorBet {
+      BetColor color;
+      uint256 bet;
+    }
+
+    struct PlayerBet {
+        address playerAddress;
+        NumberBet[37] numberBets;
+        ColorBet[2] colorBets;
+        uint256 chipsWon;
+    }
+
+    struct Child {
+        uint ed;
+    } 
+
+    struct Parent { 
+        mapping(uint => Child) children;
+        uint childrenSize;
+    }
+
+    CryptoCasinoInterface casino;
+    PlayerBet[2] public roulettePlayers;
+
+    uint numPlayers;
 
     constructor (address casinoAddress) {
-        _casinoAddress = casinoAddress;
+        casino = CryptoCasinoInterface(casinoAddress);
+        numPlayers = 0;
     }
 
-    event RouletteRolled(uint8 number);
+    event RouletteRolled(address winner, uint8 number);
     event PlayerAdded(uint8 currentPlayersCount);
     
-    function addPlayerToRoulette() public {
-        PlayerBets storage player;
-        player.playerAddress = msg.sender;
+   
+
+    function addPlayerToRoulette(NumberBet[2] memory numbBets, uint8 numbBetsSize, ColorBet[1] memory colBets, uint8 colBetsSize) public {
+        require(numPlayers <= 2, "Table is full, come back later.");
+        uint256 quantity = 0;
+        for(uint8 j = 0; j < numbBetsSize; j++){
+            quantity += numbBets[j].bet;
+        }
+
+        for(uint8 j = 0; j < colBetsSize; j++){
+            quantity += colBets[j].bet;
+        }
+        casino.transferFrom(msg.sender, quantity);
+        PlayerBet storage currentPlayer = roulettePlayers[numPlayers++];
+        currentPlayer.playerAddress = msg.sender;
+        currentPlayer.chipsWon = 0;
+
+        for(uint8 j = 0; j < numbBetsSize; j++){
+            currentPlayer.numberBets[j] = NumberBet(numbBets[j].number, numbBets[j].bet);
+        }
+
+        for(uint8 j = 0; j < colBetsSize; j++){
+            currentPlayer.colorBets[j] = ColorBet(colBets[j].color, colBets[j].bet);
+        }
+
+        if (numPlayers == 2){
+            casino.updateRandomNumber();
+        }
     }
-
-
 
     function execute(uint256 randomNumber) override external {
+      uint8 rouletteNumber = uint8(randomNumber % 37 + 1);
+      address winner = address(0);
+      for(uint8 i = 0; i < 1; i++){
+        PlayerBet storage playerBet = roulettePlayers[i];
+        NumberBet[37] storage numbBets = playerBet.numberBets;
+        for(uint8 j = 0; j < numbBets.length; j++){
+          if(numbBets[j].number == rouletteNumber) {
+            playerBet.chipsWon = playerBet.chipsWon + numbBets[j].bet * 2;
+          }
+        }
 
+        ColorBet[2] storage colBets = playerBet.colorBets;
+        for(uint8 j = 0; j < colBets.length; j++){
+          BetColor rouletteColor = BetColor(rouletteNumber%1);
+          if(colBets[j].color == rouletteColor) {
+            playerBet.chipsWon = playerBet.chipsWon + colBets[j].bet * 2;
+          }
+        }
+        casino.transfer(playerBet.playerAddress, playerBet.chipsWon);
+      }
+
+      numPlayers = 0;
+
+      emit RouletteRolled(winner, rouletteNumber);
     }
-
 }
